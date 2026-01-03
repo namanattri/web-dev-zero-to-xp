40e7bc3e56b42022e7df8a91687d3b7aeaeab885
Chander Sagar <csasagar1@gmail.com>
2026-01-03 06:38:46 -0600

    2nd answer solved of assignment4

---
+++ submissions/csa/assignment4/assignment4_Answers2.py
@@ -0,0 +1,25 @@
+# # Assignment 4: Loops (for and while)
+
+# Complete the following coding challenges to practice iteration with loops.
+
+# ---
+# ## Question 2: Sum of First N Numbers
+# Calculate the sum of first N natural numbers using a loop.
+
+# **Sample Input:**
+# ```
+# n = 10
+# ```
+
+# **Sample Output:**
+# ```
+# Sum of first 10 numbers: 55
+# ```
+print("Answer 2: Sum of First N Numbers")
+
+n = 10
+
+total = 0
+for i in range(1,n+1):
+    total += i
+print(f"Sum of first 10 numbers: {total}")
